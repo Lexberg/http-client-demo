ad2a4b5f7fa3a741fe9fcce0005b1a4463df206a
Lexberg <martin.a.lexberg@hotmail.com>
2020-09-09 13:37:43 +0200

    new test ( Failed)

--- src/test/java/no/kristiania/httpclient/QueryStringTest.java
+++ src/test/java/no/kristiania/httpclient/QueryStringTest.java
@@ -9,5 +9,11 @@ public class QueryStringTest {
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
     }
+
+    @Test
+    void shouldRetrieveOtherQueryParameter(){
+        QueryString queryString = new QueryString("status=200");
+        assertEquals("404", queryString.getParameter("status"));
+    }
 }
 
